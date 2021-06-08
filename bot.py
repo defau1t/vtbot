ee541b2b8569697f64aaacb624b3dbd37127388a
Robin Lowe <robin5693@gmail.com>
2021-06-07 22:55:04 -0600

    Bot now properly responds to command

--- bot.py
+++ bot.py
@@ -13,4 +13,18 @@ bot = commands.Bot(command_prefix='/')
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
+@bot.command(name='99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
 bot.run(TOKEN)
