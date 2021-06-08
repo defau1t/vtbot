1b78786ccac49ce535ea81a2fd275664c7901111
Robin Lowe <robin5693@gmail.com>
2021-06-07 22:31:38 -0600

    Bot now responds to simple '99!' command.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -21,4 +22,23 @@ async def on_member_join(member):
     await member.dm_channel.send(
             f'Hi {member.name}, welcome to my Discord server!'
     )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+            'I\'m the human form of the 💯 emoji.',
+            'Bingpot!',
+            (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+            ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
 client.run(TOKEN)
