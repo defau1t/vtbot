587ee8e3eac2ff06e95a0bbc52069e7f3d95c397
Robin Lowe <robin5693@gmail.com>
2021-06-07 22:17:43 -0600

    For loop replaced with lambda using discord.utils.find

--- bot.py
+++ bot.py
@@ -13,15 +13,9 @@ intents.members = True
 client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(f'{client.user} is connected to the following guild:\n'
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    print(
+            f'{client.user} is connected to the following guild:\n'
             f'{guild.name}(id: {guild.id})'
             )
-
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
-
 client.run(TOKEN)
