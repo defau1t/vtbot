7540f0672e351f093d2628b8f6ca73df53e985ac
Robin Lowe <robin5693@gmail.com>
2021-06-07 22:15:14 -0600

    Bot is now able to read all users thanks to https://stackoverflow.com/questions/64231025/discord-py-bot-cant-see-members

--- bot.py
+++ bot.py
@@ -7,7 +7,10 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     for guild in client.guilds:
